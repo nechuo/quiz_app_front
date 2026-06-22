1483df2b13789fe6ae32cf34a10cfb826834e6de
CorentinVernet <corentin.informatique89@gmail.com>
2026-06-22 18:27:14 +0200

    Add home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: .center,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 0),
+              child: Text(
+                'Welcome to Quiz App',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10.0, bottom: 20.0),
+              child: Text(
+                'Sign In to continue',
+                style: TextStyle(fontSize: 19),
+              ),
+            ),
+
+            Container(
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your username',
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
+                onChanged: (text) {},
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter your password',
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+                onChanged: (text) {},
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 30.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 157, 110, 237),
+                    foregroundColor: Colors.white,
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: const EdgeInsets.only(top: 15, bottom: 15),
+                  ),
+                  child: Text('Login'),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 157, 110, 237),
+                        fontSize: 18,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  InkWell(
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 157, 110, 237),
+                        fontSize: 18,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
